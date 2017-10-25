a0fb473988d13e98689d60871ed38b85b55e24fb
yas15 <31019957+yas15@users.noreply.github.com>
2017-10-25 21:27:05 +0100

    4th Submit

--- App2/Resources/Resource.Designer.cs
+++ App2/Resources/Resource.Designer.cs
@@ -45,10 +45,13 @@ namespace App2
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int Apu = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Robot_Pic = 2130837505;
+			public const int Icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Robot_Pic = 2130837506;
 			
 			static Drawable()
 			{
